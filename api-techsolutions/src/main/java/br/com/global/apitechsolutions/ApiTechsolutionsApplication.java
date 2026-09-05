99a110608c1513fabe862dbc25801c10cedd14b6
Tamires Cordeiro <mirescordeiro@gmail.com>
2026-09-05 09:17:04 -0300

    chore: inicializa o projeto

---
+++ api-techsolutions/src/main/java/br/com/global/apitechsolutions/ApiTechsolutionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.global.apitechsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTechsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTechsolutionsApplication.class, args);
+	}
+
+}
---
+++ api-techsolutions/src/test/java/br/com/global/apitechsolutions/ApiTechsolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.global.apitechsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTechsolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
